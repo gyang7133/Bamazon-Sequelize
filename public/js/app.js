f868cbfbba51f07c0666bf76fb84a83ffb19d982
Genie Yang <genieyang93@gmail.com>
2019-01-13 23:33:43 -0500

    Edit app.js

--- public/js/app.js
+++ public/js/app.js
@@ -33,6 +33,8 @@ const validate = function(item) {
   }
 };
 
+
+
 //this is the document ready function that says to render all the items in the database, to the table.
 $(document).ready(() => {
   $.ajax({
